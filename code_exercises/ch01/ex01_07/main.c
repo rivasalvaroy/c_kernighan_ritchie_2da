1917c3a7d73f2301ad354d17f6272b3084f47553
rivasalvaroy <rivasalvaroy@gmail.com>
2022-08-28 11:16:16 -0500

    ex01_07 - exercise 1-7

---
+++ code_exercises/ch01/ex01_07/main.c
@@ -0,0 +1,10 @@
+// Escriba un programa que imprima el valor de EOF
+#include <stdio.h>
+
+// la función main empieza la ejecución del programa
+int main()
+{
+   printf("EOF is %d\n", EOF);
+   printf("ENTER is %d\n", '\n');
+   return 0;
+}
