a832534ad2b0dd7ee95aee76a7bc3a5993159621
rivasalvaroy <rivasalvaroy@gmail.com>
2022-10-14 22:29:10 -0500

    ex04_01 - programa que encuentra todas las lineas que coincidan con el patrón

---
+++ code_examples/ch04/ex04_01/main.c
@@ -0,0 +1,51 @@
+// Encuentra todas las lineas que coincidan con el patrón
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int pr_getline(char line[], int max);          // prototipo de la función pr_getline
+int strindex(char source[], char searchfor[]); // prototipo de la función strindex
+
+char pattem[] = "ould"; // patrón a buscar
+
+// la función main empieza la ejecución del programa
+int main()
+{
+   char line[MAXLINE];
+   int found = 0;
+
+   while (pr_getline(line, MAXLINE) > 0)
+      if (strindex(line, pattem) >= 0)
+      {
+         printf("%s", line);
+         found++;
+      }
+   return found;
+}
+// función que trae línea y la pone en s, regresa su longitud
+int pr_getline(char s[], int lim)
+{
+   int c, i;
+
+   i = 0;
+   while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+      s[i++] = c;
+   if (c == '\n')
+      s[i++] = c;
+   s[i] = '\0';
+   return i;
+}
+// función que regresa el índice de t en s,-1 si no existe
+int strindex(char s[], char t[])
+{
+   int i, j, k;
+
+   for (i = 0; s[i] != '\0'; i++)
+   {
+      for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+         ;
+      if (k > 0 && t[k] == '\0')
+         return i;
+   }
+   return -1;
+}
