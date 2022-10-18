ea651d4aaedb0a9b49419408d923dcaba518619e
rivasalvaroy <rivasalvaroy@gmail.com>
2022-10-18 09:37:12 -0500

    ex04_02 - exercise 4-1, 2nd version

---
+++ code_exercises/ch04/ex04_02/main.c
@@ -0,0 +1,54 @@
+/* Escriba la función strrindex(s,t), que regresa la posición de la ocu­rrencia 
+   de más a la derecha de t en s, ó -1 si no hay alguna. Mas eficiente */
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 100
+
+int pr_getline(char line[], int max);           // prototipo de la función pr_getline
+int strrindex(char source[], char searchfor[]); // prototipo de la función strrindex
+
+char pattem[] = "ould"; // patrón a buscar
+
+// la función main empieza la ejecución del programa
+int main()
+{
+   char line[MAXLINE];
+   int found = 0;
+
+   while (pr_getline(line, MAXLINE) > 0)
+      if (strrindex(line, pattem) >= 0)
+      {
+         printf("%s", line);
+         found++;
+      }
+   return found;
+}
+// función que trae línea y la pone en s, regresa su longitud
+int pr_getline(char s[], int lim)
+{
+   int c, i;
+
+   i = 0;
+   while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+      s[i++] = c;
+   if (c == '\n')
+      s[i++] = c;
+   s[i] = '\0';
+   return i;
+}
+// función que regresa la posición de la ocu­rrencia de más a la derecha de t en s,
+// ó -1 si no hay alguna
+int strrindex(char s[], char t[])
+{
+   int i, j, k;
+
+   for (i = strlen(s) - strlen(t); i >= 0; i--)
+   {
+      for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+         ;
+      if (k > 0 && t[k] == '\0')
+         return i;
+   }
+   return -1;
+}
